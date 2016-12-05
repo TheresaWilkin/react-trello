21a5fc5476b4440824c7f9997169d96f4b14f60e
Theresa Wilkin <theresawilkin@yahoo.com>
2016-12-05 00:16:01 -0500

    init

---
+++ js/components/card.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default function Person() {
+    return (
+        <div className="card">
+            <p>This is a card</p>
+        </div>
+    );
+}
